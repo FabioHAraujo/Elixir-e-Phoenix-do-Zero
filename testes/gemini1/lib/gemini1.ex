6d24ee234b5c5d3cbcbfd5b1fbaf646c8bc8c154
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-06-27 18:39:02 -0300

    Boooora

--- testes/gemini1/lib/gemini1.ex
+++
@@ -1,7 +0,0 @@
-defmodule Gemini1 do
-  def main(_) do
-    Lista.start_link(nil)
-    Enum.each(1..2, &Produtor.iniciar/1)
-    Enum.each(1..2, &Consumidor.iniciar/1)
-  end
-end
--- testes/gemini1/test/gemini1_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Gemini1Test do
-  use ExUnit.Case
-  doctest Gemini1
-
-  test "greets the world" do
-    assert Gemini1.hello() == :world
-  end
-end
